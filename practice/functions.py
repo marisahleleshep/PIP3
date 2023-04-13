0e3221bf24fc1b8c5e777ad3afdac4e4a4031c8b
marisahleleshep <leleshepmmarisaa@gmail.com>
2023-04-13 20:23:57 +0300

    first commit

---
+++ practice/functions.py
@@ -0,0 +1,22 @@
+def myself(name,age):
+    year=2023-age
+    print(f"My name is {name} and I was born in {year} ")
+
+def my_school(name,location):
+    print(f"My school is {name} and is located in {location}")    
+
+def hello(name):
+    print(f"My name is {name}")
+
+def about_me(name,year,country,identity):
+    age=2023-year
+    print(f"My name is {name}, I am {age} years old from {country} and am a {identity}")
+
+
+
+
+
+
+
+
+
---
+++ practice/math.py
@@ -0,0 +1,27 @@
+def addition(a,b):
+    answer=a+b
+    return answer
+
+def multiplication(d,c):
+    result=d*c
+    return result
+
+def even(num):
+    if num %2 == 0:
+        return True
+    else:
+        return False    
+
+def multipl(x):
+    answer=5* x
+    return answer
+def division(m,k):
+    output=m / k
+    return output
+
+def remainder(y,p):
+    answer=y % p
+    return answer    
+
+
+
